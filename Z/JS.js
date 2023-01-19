64dcad9a6e5d8013d35c9776ee4ee5ff48c34cca
A31F <affdh01@gmail.com>
2023-01-19 15:42:13 +0700

    PiH

---
+++ Z/JS.js
@@ -0,0 +1,15 @@
+//menu aktif
+const mena = document.querySelector(".menu");
+// Ketika Aktif
+document.querySelector("#mn").onclick = () => {
+  mena.classList.toggle("active");
+};
+
+// Klik Diluar
+const mnl = document.querySelector("#mn");
+
+document.addEventListener("click", function (e) {
+  if (!mnl.contains(e.target) && !mena.contains(e.target)) {
+    mena.classList.remove("active");
+  }
+});
